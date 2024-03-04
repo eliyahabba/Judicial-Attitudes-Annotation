331417b30b5522d4a714396d1b558ee67738c240
eliyahabba <eliyahabba@gmail.com>
2024-03-04 10:51:02 +0200

    fixed params names

--- src/LegalAnnotationSentencesPage/InstructionsDisplayerEnglish.py
+++ src/LegalAnnotationSentencesPage/InstructionsDisplayerEnglish.py
@@ -1,75 +1,74 @@
 import streamlit as st
-
 import utils.font_utils as fu
 
 
 class InstructionsDisplayerEnglish:
     @staticmethod
     def display_instructions():
-        # display to screen instructions for the user to annotate the text
-        instructions = f'Please label the attached text by selecting the appropriate option. Once you have finished tagging the text, ' \
-                       f'click on the "Save Tagging" button. '
+        # Display instructions for the user to annotate the text
+        main_instructions = (
+            "Please label the attached text by selecting the appropriate option. "
+            "Once you have finished tagging the text, click on the 'Save Tagging' button."
+        )
 
-        instructions2 = """
-        If the sentence does not pertain to the impression or credibility of the complainant, you may choose:
-        """
-        instructions3 = 'not relevant'
+        additional_instructions = (
+            "If the sentence does not pertain to the impression or credibility of the complainant, "
+            "you may choose:"
+        )
+        option_not_relevant = "not relevant"
 
-        english_instructions = 'Select the appropriate label for the text.' \
-                               ' When you finish tagging the text, click the "Save tagging" button.'
-        # display the instructions to the user in Hebrew and English languages one after the other
+        english_instructions = (
+            "Select the appropriate label for the text. "
+            "When you finish tagging the text, click the 'Save tagging' button."
+        )
+
+        # Display the instructions to the user in Hebrew and English languages one after the other
         fu.create_hebrew_style()
 
         st.markdown(
-            f'<p style= font-size:14px;  color:MediumVioletRed; direction: ltr; unicode-bidi:'
-            f' bidi-override;">{instructions}</p>',
-            unsafe_allow_html=True)
+            f'<p style="font-size:14px; color:MediumVioletRed; direction: ltr; unicode-bidi:'
+            f' bidi-override;">{main_instructions}</p>',
+            unsafe_allow_html=True,
+        )
         st.markdown(
-            f'<p style="font-size:14px;color:MediumVioletRed ; font-weight:bold; direction: ltr; unicode-bidi:'
-            f' bidi-override;">{instructions2}</p>',
-            unsafe_allow_html=True)
+            f'<p style="font-size:14px; color:MediumVioletRed; font-weight:bold; direction: ltr; '
+            f'unicode-bidi: bidi-override;">{additional_instructions}</p>',
+            unsafe_allow_html=True,
+        )
         st.markdown(
-            f'<p style="font-size:14px; color:DarkTurquoise ; font-weight:bold; direction: ltr; unicode-bidi:'
-            f' bidi-override;">{instructions3}</p>',
-            unsafe_allow_html=True)
-        st.markdown(
-            f'<p style="font-size:14px; color:MediumVioletRed  ;direction: ltr; unicode-bidi:'
-            f' bidi-override;">{english_instructions}</p>',
-            unsafe_allow_html=True)
+            f'<p style="font-size:14px; color:DarkTurquoise; font-weight:bold; direction: ltr; unicode-bidi:'
+            f' bidi-override;">{option_not_relevant}</p>',
+            unsafe_allow_html=True,
+        )
 
 
-        # context_instructions = \
-        #     """
-        #     Use the bottoms:
-        #     """
-        context_instructions2 = \
-            """
-            Use the bottoms: Add the previous sentence, Remove the first sentence, Add the next sentence, Remove the last sentence
-            """
+        # Context instructions
+        context_instructions = (
+            "Use the buttons: 'Add the previous sentence', 'Remove the first sentence', "
+            "'Add the next sentence', 'Remove the last sentence' to control the context of the tagged sentence. "
+            "The tagged sentence will appear in "
+        )
+        bold_text = "bold"
+        context_instructions_continued = (
+            " font, while the rest of the sentences will appear in normal font. "
+            "As part of the tagging process, the sentences of the context that you have chosen to display "
+            "will also be saved."
+        )
 
-        context_instructions3 = \
-            """
-            to control the context of the tagged sentence.
-            The tagged sentence will appear in
-            """
-        context_instructions4 = \
-        """
-        bold
-        """
-        context_instructions5 = \
-        """
-        font, while the rest of the sentences will appear in normal font.
-         As part of the tagging process, the sentences of the context that you have chosen to display will also be saved.
-        """
-
-        context_general_instructions  = "navigate bottom exsplanation"
+        context_general_instructions = "Navigate bottom explanation"
         with st.expander(context_general_instructions):
             st.markdown(
-                f'<p style="font-size:14px;color:MediumBlue   ; font-weight:bold;  direction: ltr; unicode-bidi:'
-                f' bidi-override;">{context_instructions2}</p>',
-                unsafe_allow_html=True)
-            # f" direction: LTR; color: grey; '>{prefix} <span style=font-weight:bold;>{real_answer} </span>{suffix}</p>",
+                f'<p style="font-size:14px; color:MediumBlue; font-weight:bold; direction: ltr; '
+                f'unicode-bidi: bidi-override;">{context_instructions}</p>',
+                unsafe_allow_html=True,
+            )
             st.markdown(
-                f"<p style='font-size:14px;color:MediumBlue ; direction: ltr; unicode-bidi:"
-                f"bidi-override;'>{context_instructions3} <span style=font-weight:bold;>{context_instructions4} </span>{context_instructions5}</p>",
-                unsafe_allow_html=True)
+                f"<p style='font-size:14px; color:MediumBlue; direction: ltr; unicode-bidi:"
+                f"bidi-override;'>{bold_text}</p>",
+                unsafe_allow_html=True,
+            )
+            st.markdown(
+                f"<p style='font-size:14px; color:MediumBlue; direction: ltr; unicode-bidi:"
+                f"bidi-override;'>{context_instructions_continued}</p>",
+                unsafe_allow_html=True,
+            )
--- src/LegalAnnotationSentencesPage/InstructionsDisplayerHebrew.py
+++ src/LegalAnnotationSentencesPage/InstructionsDisplayerHebrew.py
@@ -1,73 +1,57 @@
 import streamlit as st
-
 import utils.font_utils as fu
-
 
 class InstructionsDisplayerHebrew:
     @staticmethod
     def display_instructions():
-        # display to screen instructions for the user to annotate the text
-        instructions = 'בחרו את התווית המתאימה לטקסט המצורף. כאשר תסיימו לתייג את הטקסט, לחצו על הכפתור "שמור תיוג".'
-        instructions2 = """
-        אם המשפט כלל אינו רלוונטי (כלומר, הוא אינו מתייחס לרושם או לאמינות של המתלוננת) תוכלו להשתמש בערך:
-        """
-        instructions3 = 'not relevant'
+        # Display instructions for the user to annotate the text
+        main_instructions = (
+            "בחרו את התווית המתאימה לטקסט המצורף. כאשר תסיימו לתייג את הטקסט, לחצו על הכפתור 'שמור תיוג'."
+        )
 
-        english_instructions = 'Select the appropriate label for the text.' \
-                               ' When you finish tagging the text, click the "Save tagging" button.'
-        # display the instructions to the user in Hebrew and English languages one after the other
+        additional_instructions = (
+            "אם המשפט כלל אינו רלוונטי (כלומר, הוא אינו מתייחס לרושם או לאמינות של המתלוננת) תוכלו להשתמש "
+            "בערך:"
+        )
+        option_not_relevant = "not relevant"
+
         fu.create_hebrew_style()
 
         st.markdown(
-            f'<p style="text-align: right; font-size:16px;  color:MediumVioletRed; direction: rtl; unicode-bidi:'
-            f' bidi-override;">{instructions}</p>',
-            unsafe_allow_html=True)
+            f'<p style="text-align: right; font-size:16px; color:MediumVioletRed; direction: rtl; unicode-bidi: bidi-override;">'
+            f"{main_instructions}</p>",
+            unsafe_allow_html=True,
+        )
         st.markdown(
-            f'<p style="text-align: right; font-size:18px;color:MediumVioletRed ; font-weight:bold; direction: rtl; unicode-bidi:'
-            f' bidi-override;">{instructions2}</p>',
-            unsafe_allow_html=True)
+            f'<p style="text-align: right; font-size:18px; color:MediumVioletRed; font-weight:bold; direction: rtl; '
+            f'unicode-bidi: bidi-override;">{additional_instructions}</p>',
+            unsafe_allow_html=True,
+        )
         st.markdown(
-            f'<p style="text-align: right; font-size:16px; color:DarkTurquoise ; font-weight:bold; direction: ltr; unicode-bidi:'
-            f' bidi-override;">{instructions3}</p>',
-            unsafe_allow_html=True)
-        st.markdown(
-            f'<p style="text-align: right; font-size:16px; color:MediumVioletRed  ;direction: ltr; unicode-bidi:'
-            f' bidi-override;">{english_instructions}</p>',
-            unsafe_allow_html=True)
+            f'<p style="text-align: right; font-size:16px; color:DarkTurquoise; font-weight:bold; direction: ltr; unicode-bidi:'
+            f' bidi-override;">{option_not_relevant}</p>',
+            unsafe_allow_html=True,
+        )
 
-        context_instructions = \
-        """
-        תשתמשו בכפתורים:
-        Add the previous sentence
-        Remove the first sentence
-        Add the next sentence
-        Remove the last sentence
-        
-        בשביל לשלוט בהקשר של המשפט המתויג. 
-        המשפט המתויג מופיע בטקסט bold, ושאר המשפטים יופיעו בגופן רגיל. 
-        כחלק מהשמירה של התיוג, נשמרים גם המשפטים של ההקשר שבחרתם להציג בשביל לתייג בצורה נכונה. 
-        לכן לפני שאתם שומרים, שימו לב שהמשפטים המופיעים לפני ואחרי המשפט באמת נדרשים בשביל ההקשר של המשפט
-        """
-
-        context_instructions = \
+        context_instructions_general = \
             """
             תשתמשו בכפתורים:
             """
-        context_instructions2 = \
+        context_instructions_bottoms = \
             """
             Add the previous sentence, Remove the first sentence, Add the next sentence, Remove the last sentence
             """
 
-        context_instructions3 = \
+        context_instructions_control = \
             """
             בשביל לשלוט בהקשר של המשפט המתויג.
             המשפט המתויג מופיע בטקסט
             """
-        context_instructions4 = \
+        context_instructions_bold = \
         """
         dlob
         """
-        context_instructions5 = \
+        context_instructions_explain = \
         """
             , ושאר המשפטים יופיעו בגופן רגיל.
             כחלק מהשמירה של התיוג, נשמרים גם המשפטים של ההקשר שבחרתם להציג בשביל לתייג בצורה נכונה.
@@ -78,15 +62,15 @@ class InstructionsDisplayerHebrew:
         with st.expander(context_general_instructions):
             st.markdown(
                 f'<p style="text-align: right; font-size:18px;color:MediumBlue  ; direction: rtl; unicode-bidi:'
-                f' bidi-override;">{context_instructions}</p>',
+                f' bidi-override;">{context_instructions_general}</p>',
                 unsafe_allow_html=True)
 
             st.markdown(
                 f'<p style="text-align: right; font-size:18px;color:MediumBlue   ; font-weight:bold;  direction: ltr; unicode-bidi:'
-                f' bidi-override;">{context_instructions2}</p>',
+                f' bidi-override;">{context_instructions_bottoms}</p>',
                 unsafe_allow_html=True)
             # f" direction: RTL; color: grey; '>{prefix} <span style=font-weight:bold;>{real_answer} </span>{suffix}</p>",
             st.markdown(
                 f"<p style='text-align: right; font-size:18px;color:MediumBlue ; direction: rtl; unicode-bidi:"
-                f"bidi-override;'>{context_instructions3} <span style=font-weight:bold;>{context_instructions4} </span>{context_instructions5}</p>",
+                f"bidi-override;'>{context_instructions_control} <span style=font-weight:bold;>{context_instructions_bold} </span>{context_instructions_explain}</p>",
                 unsafe_allow_html=True)
